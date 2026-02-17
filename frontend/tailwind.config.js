8dc8faeb4e72c843055ff826f1b01b2e97df7220
Sahil Sanas <sahilhsanas22@gmail.com>
2026-02-17 21:08:28 +0530

    .env removed

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: '#09637E',
+          DEFAULT: '#088395',
+          light: '#7AB2B2',
+        },
+        accent: '#7AB2B2',
+        background: '#EBF4F6',
+        teal: {
+          900: '#09637E',
+          700: '#088395',
+          400: '#7AB2B2',
+          50: '#EBF4F6',
+        },
+      },
+    },
   },
   plugins: [],
 }
